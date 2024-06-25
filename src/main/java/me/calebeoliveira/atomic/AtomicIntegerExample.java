3b3b624b8017e8fc752fa728c5289346067a82e4
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-06-25 09:49:00 -0300

    add atomic integer example

---
+++ src/main/java/me/calebeoliveira/atomic/AtomicIntegerExample.java
@@ -0,0 +1,62 @@
+package me.calebeoliveira.atomic;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerExample {
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+
+        incrementingThread.join();
+        decrementingThread.join();
+
+        System.out.printf("We currently have %s items%n", inventoryCounter.getItems());
+    }
+    private static class DecrementingThread extends Thread {
+        private final InventoryCounter inventoryCounter;
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    private static class IncrementingThread extends Thread {
+        private final InventoryCounter inventoryCounter;
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+    private static class InventoryCounter {
+        private AtomicInteger items = new AtomicInteger(0);
+
+        public void increment() {
+            items.incrementAndGet();
+        }
+
+        public void decrement() {
+            items.decrementAndGet();
+        }
+
+        public int getItems() {
+            return items.get();
+        }
+    }
+}
